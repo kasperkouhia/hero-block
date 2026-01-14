745a5cc8592c6b210e28fe3dea8914c66f300067
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2026-01-14 21:36:20 +0200

    Update version number

--- hero-block.php
+++ hero-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Hero Block
  * Description:       A simple custom hero block for WordPress.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            Kasper Kouhia
