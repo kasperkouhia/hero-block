e0642a58d486dbedb488beee2a6a086f7276fa33
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2026-01-22 14:12:04 +0200

    Build version 1.0.4

--- build/hero-block/index.asset.php
+++ build/hero-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f2d068872ed99edefedf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fa77b296f21218a92f0c');
